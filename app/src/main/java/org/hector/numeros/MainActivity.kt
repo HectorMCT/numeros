15120c5785e33ed4430ff93f2812369c14d1ba57
Hector Manuel Chavez Troncoso <hectorchavezt@hotmail.com>
2021-11-29 23:13:32 -0600

    change visibility variable

--- app/src/main/java/org/hector/numeros/MainActivity.kt
+++ app/src/main/java/org/hector/numeros/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    var numeros = mutableListOf<String>()
+    private var numeros = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
